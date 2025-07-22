47697a036ed884d50c5d4add6bd9e3a9e70218f5
Mostafa Ibrahim <11410120200534@stud.cu.edu.eg>
2025-07-22 15:43:29 +0300

    Time: 103 ms (57.02%), Space: 48.9 MB (17.37%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        HashSet<ListNode> set = new();
+        var curr= head;
+        while(curr is not null){
+            if(set.Contains(curr)) return true;
+            set.Add(curr);
+            curr = curr.next;
+        }
+        return false;
+    }
+}
